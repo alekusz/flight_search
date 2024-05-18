950fa63d3061a988afba8f5270f803e1359ec8be
Bubu <bubuy2012@gmail.com>
2024-05-18 20:16:20 +0200

    code for cleaning

---
+++ data_preprocessing/data_cleaning.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import csv
+
+# Read the appended CSV file into a DataFrame
+df = pd.read_csv('modified_flights_data.csv')
+
+# Deleting columns with >80% missing data
+df = df.drop(['TRAVEL_SERVICES_amenityProvider', 'TRAVEL_SERVICES_isChargeable', 'TRAVEL_SERVICES_description',
+              'PRE_RESERVED_SEAT_amenityProvider', 'PRE_RESERVED_SEAT_isChargeable', 'PRE_RESERVED_SEAT_description',
+              'BRANDED_FARES_amenityProvider', 'BRANDED_FARES_isChargeable', 'BRANDED_FARES_description'], axis=1)
+
+print(df)
+
+# Checking the columns: 
+# Currency.1
+unique_values = df['Currency.1'].unique()
+
+# Print the unique values
+print("Unique values:", unique_values)
+    # only currency is EUR -> delete
+
+df = df.drop('Currency.1', axis=1)
+
+# Total Price.1 -> these have the same as grand total, so we also delete this. 
+df = df.drop('Total Price.1', axis=1)
+
+# Flight departures -> turn into categorical variable
+
+# Function to extract time after 'T'
+def extract_time(datetime_str):
+    if isinstance(datetime_str, str) and 'T' in datetime_str:
+        return datetime_str.split('T')[1]
+    else:
+        return ''
+    
+# apply function to all departures and arrivals
+df['Firt flight departure'] = df['Firt flight departure'].apply(extract_time)
+df['Second flight departure'] = df['Second flight departure'].apply(extract_time)
+df['Third flight departure'] = df['Third flight departure'].apply(extract_time)
+df['Firt flight arrival'] = df['Firt flight arrival'].apply(extract_time)
+df['Second flight arrival'] = df['Second flight arrival'].apply(extract_time)
+df['Third flight arrival'] = df['Third flight arrival'].apply(extract_time)
+
+# Rename column
+df.rename(columns={'Firt flight departure': 'First flight departure'}, inplace=True)
+df.rename(columns={'Firt flight arrival': 'First flight arrival'}, inplace=True)
+
+print(df)
+
+# Creating a new column for departure and arrival times to be categorized ---------------------------
+
+# Define the function to categorize flight times
+def categorize_time(time_str):
+    hour = int(time_str.split(':')[0])
+    if 6 <= hour < 12:
+        return "Morning"
+    elif 12 <= hour < 18:
+        return "Afternoon"
+    else:
+        return "Evening"
+
+df['Departure Time'] = df['First flight departure'].apply(categorize_time)
+
+
+print(df)
+
+df.to_csv('cleaned_data.csv', index=False)
+# Display the DataFrame
+print(df['Departure Time'])
+
+
+
+
+
+
+
+
