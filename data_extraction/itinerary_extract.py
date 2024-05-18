cded3ab2a780c2870138f0728a86cae790cdd5b5
Bubu <bubuy2012@gmail.com>
2024-05-18 14:09:20 +0200

    editing to NYC flights

--- data_extraction/itinerary_extract.py
+++ data_extraction/itinerary_extract.py
@@ -12,7 +12,7 @@ def extract_fields(json_data):
     departure_durations = {}
     arrival_terminals = []
     arrival_times = []
-    aircraft_codes = []
+    airline_codes = []
     blacklists = []
     
     for segment in segments:
@@ -27,8 +27,7 @@ def extract_fields(json_data):
         arrival_terminals.append(arrival.get("terminal", None))
         arrival_times.append(arrival.get("at", None))
 
-        aircraft = segment.get("aircraft", {})
-        aircraft_codes.append(aircraft.get("code", None))
+        airline_codes.append(segment.get("carrierCode", False))
 
         blacklists.append(segment.get("blacklistedInEU", False))
 
@@ -37,17 +36,17 @@ def extract_fields(json_data):
     departure_times_str = ', '.join(str(at) if at is not None else "" for at in departure_times)
     arrival_terminals_str = ', '.join(str(terminal) if terminal is not None else "" for terminal in arrival_terminals)   
     arrival_times_str = ', '.join(str(at) if at is not None else "" for at in arrival_times)
-    aircraft_codes_str = ', '.join(str(code) if code is not None else "" for code in aircraft_codes)
+    airline_codes_str = ', '.join(str(carrierCode) if carrierCode is not None else "" for carrierCode in airline_codes)
     blacklists_str = ', '.join(str(blacklist) for blacklist in blacklists)
 
     number_of_stops = max(0, len(segments) - 1)
     
-    return duration, iataCodes_str, terminals_str, number_of_stops, departure_durations, arrival_terminals_str, departure_times_str, arrival_times_str, aircraft_codes_str, blacklists_str
+    return duration, iataCodes_str, terminals_str, number_of_stops, departure_durations, arrival_terminals_str, departure_times_str, arrival_times_str, airline_codes_str, blacklists_str
 
 # Read CSV file and process each row
-with open('flight_search.csv', mode='r') as csv_file:
+with open('flight_nyc.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
-    output_headers = ['Duration', 'Departure IATACodes', 'Departure Terminals', 'Number of Stops', 'layover durations', 'Arrival Terminals', 'Departure Times', 'Arrival Times', 'Aircraft', 'Blacklist']
+    output_headers = ['Duration', 'Departure IATACodes', 'Departure Terminals', 'Number of Stops', 'layover durations', 'Arrival Terminals', 'Departure Times', 'Arrival Times', 'Airline', 'Blacklist']
     
     with open('itinerary.csv', mode='w', newline='') as output_file:
         writer = csv.writer(output_file)
@@ -57,6 +56,6 @@ with open('flight_search.csv', mode='r') as csv_file:
             itinerary_json = row['itineraries']
             itinerary_data = json.loads(itinerary_json)[0]
             
-            duration, departure_iataCodes, departure_terminals, number_of_stops, departure_durations, arrival_terminal, departure_times, arrival_times, aircraft_codes, blacklists = extract_fields(itinerary_data)
+            duration, departure_iataCodes, departure_terminals, number_of_stops, departure_durations, arrival_terminal, departure_times, arrival_times, airline_codes, blacklists = extract_fields(itinerary_data)
             
-            writer.writerow([duration, departure_iataCodes, departure_terminals, number_of_stops, departure_durations, arrival_terminal, departure_times, arrival_times, aircraft_codes, blacklists])
+            writer.writerow([duration, departure_iataCodes, departure_terminals, number_of_stops, departure_durations, arrival_terminal, departure_times, arrival_times, airline_codes, blacklists])
