3be87c8eb73f169e60c90e75727373a184feb52b
Bubu <bubuy2012@gmail.com>
2024-05-20 21:31:44 +0200

    commit

--- Models/RF_model.py
+++ Models/RF_model.py
@@ -23,7 +23,7 @@ y = df['Log Grand Total Price']
 X_encoded = pd.get_dummies(X)
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
 
 # Instantiate the Random Forest Regressor
 rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
