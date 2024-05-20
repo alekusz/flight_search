362c52da46d55c1addf16528fb1a4d312a4d8a41
Bubu <bubuy2012@gmail.com>
2024-05-20 21:14:57 +0200

    update with rmse

--- Models/RF_model.py
+++ Models/RF_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, root_mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
@@ -37,12 +37,12 @@ y_pred = rf_model.predict(X_test)
 
 # Evaluate the model
 mse = mean_squared_error(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
+rmse = root_mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
 # Print the evaluation results
 print(f"Mean Squared Error: {mse:.2f}")
-print(f"Mean Absolute Error: {mae:.2f}")
+print(f"Root Mean Squared Error: {rmse:.2f}")
 print(f"R^2 Score: {r2:.2f}")
 
 # Get feature importances
