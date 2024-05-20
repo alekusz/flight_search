97bd99f555b3de9a997b21c4c3e7c3fa0bc87b9b
Bubu <bubuy2012@gmail.com>
2024-05-20 20:50:15 +0200

    random forest model

---
+++ RF_model.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+current_directory = os.getcwd()
+print(current_directory)
+
+# Change the current working directory
+os.chdir("Feature_engineering")
+
+# Read the CSV file
+df = pd.read_csv('final_data.csv')
+
+# Creating the matrix
+X = df.drop(columns=['Log Grand Total Price', 'Grand Total Price'])
+y = df['Log Grand Total Price']
+
+# One-Hot Encode the categorical columns
+X_encoded = pd.get_dummies(X)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.3, random_state=42)
+
+# Instantiate the Random Forest Regressor
+rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+# Train the Random Forest model
+rf_model.fit(X_train, y_train)
+
+
+# Make predictions on the test set
+y_pred = rf_model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+mae = mean_absolute_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+# Print the evaluation results
+print(f"Mean Squared Error: {mse:.2f}")
+print(f"Mean Absolute Error: {mae:.2f}")
+print(f"R^2 Score: {r2:.2f}")
+
+# Get feature importances
+importances = rf_model.feature_importances_
+
+# Create a DataFrame for better visualization
+feature_importances = pd.DataFrame({'Feature': X_encoded.columns, 'Importance': importances})
+
+# Sort the DataFrame by importance
+feature_importances = feature_importances.sort_values(by='Importance', ascending=False)
+
+# Plot the feature importances
+plt.figure(figsize=(10, 6))
+sns.barplot(x='Importance', y='Feature', data=feature_importances.head(10))  # Displaying top 10 features
+plt.title('Feature Importances')
+plt.show()
+
